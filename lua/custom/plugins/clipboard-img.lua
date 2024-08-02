5dccc53dbee2ef8a5147cb8601d40dc96fc5d04a
Codecrafter_404 <codecrafter404@github.com>
2024-08-02 22:25:34 +0200

    REVERT: remove clipboard image dir

--- lua/custom/plugins/clipboard-img.lua
+++ lua/custom/plugins/clipboard-img.lua
@@ -1,13 +1,14 @@
-local function get_image_target_dir()
-  local pathseperator = '/'
-  if vim.fn.has 'win32' == 1 then
-    pathseperator = '\\'
-  end
-  local current_file_path = vim.fn.expand '%:p:h' -- Get full path of the current file
-  local cwd = vim.fn.getcwd() .. pathseperator .. 'Content'
-  local res = current_file_path:sub(string.len(cwd) + 2)
-  return 'Attachements' .. pathseperator .. res
-end
+-- this doesn't work as it only gets the path of the current file not the current buffer file
+-- local function get_image_target_dir()
+--   local pathseperator = '/'
+--   if vim.fn.has 'win32' == 1 then
+--     pathseperator = '\\'
+--   end
+--   local current_file_path = vim.fn.expand '%:p:h' -- Get full path of the current file
+--   local cwd = vim.fn.getcwd() .. pathseperator .. 'Content'
+--   local res = current_file_path:sub(string.len(cwd) + 2)
+--   return 'Attachements' .. pathseperator .. res
+-- end
 
 return {
   'codecrafter404/clipboard-image.nvim',
@@ -15,10 +16,10 @@ return {
   ft = { 'markdown' },
   config = function()
     require('clipboard-image').setup {
-      default = {
-        img_dir = get_image_target_dir(),
-        img_dir_txt = get_image_target_dir(),
-      },
+      -- default = {
+      --   img_dir = get_image_target_dir(),
+      --   img_dir_txt = get_image_target_dir(),
+      -- },
     }
     vim.keymap.set('n', '<leader>ii', '<cmd>:PasteImg<CR>', { desc = '[I]nsert [I]mage from clipboard' })
   end,
