807d117aa15b5ae5339ae882e722edbff1bc694d
code <code@CODE-SURFACE>
2024-08-02 21:41:33 +0200

    TASK: update path in clipboard

--- lua/custom/plugins/clipboard-img.lua
+++ lua/custom/plugins/clipboard-img.lua
@@ -1,9 +1,25 @@
+local function get_image_target_dir()
+  local pathseperator = '/'
+  if vim.fn.has 'win32' == 1 then
+    pathseperator = '\\'
+  end
+  local current_file_path = vim.fn.expand '%:p:h' -- Get full path of the current file
+  local cwd = vim.fn.getcwd() .. pathseperator .. 'Content'
+  local res = current_file_path:sub(string.len(cwd) + 2)
+  return 'Attachements' .. pathseperator .. res
+end
+
 return {
   'codecrafter404/clipboard-image.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   ft = { 'markdown' },
   config = function()
-    require('clipboard-image').setup {}
+    require('clipboard-image').setup {
+      default = {
+        img_dir = get_image_target_dir(),
+        img_dir_txt = get_image_target_dir(),
+      },
+    }
     vim.keymap.set('n', '<leader>ii', '<cmd>:PasteImg<CR>', { desc = '[I]nsert [I]mage from clipboard' })
   end,
 }
